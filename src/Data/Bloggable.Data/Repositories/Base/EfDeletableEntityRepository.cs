6ade50996d904f8d01c240eaa28a9eabeb24bc96
vladislav-karamfilov <vladislav.karamfilov@gmail.com>
2015-06-14 18:22:11 +0300

    Added base data repositories.

---
+++ src/Data/Bloggable.Data/Repositories/Base/EfDeletableEntityRepository.cs
@@ -0,0 +1,26 @@
+﻿namespace Bloggable.Data.Repositories.Base
+{
+    using System.Data.Entity;
+    using System.Linq;
+
+    using Bloggable.Data.Contracts;
+
+    public class EfDeletableEntityRepository<T> : EfRepository<T>, IDeletableEntityRepository<T>
+        where T : class, IDeletableEntity
+    {
+        public EfDeletableEntityRepository(DbContext context)
+            : base(context)
+        {
+        }
+
+        public override IQueryable<T> All()
+        {
+            return base.All().Where(x => !x.IsDeleted);
+        }
+
+        public IQueryable<T> AllWithDeleted()
+        {
+            return base.All();
+        }
+    }
+}
