5bd44fc717864c24c2347434dcaaad7d54837f8f
vladislav-karamfilov <vladislav.karamfilov@gmail.com>
2015-06-14 16:32:24 +0300

    Initial structure.

---
+++ Web/Bloggable.Web/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Bloggable.Web.Startup))]
+namespace Bloggable.Web
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
