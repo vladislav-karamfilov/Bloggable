815bf1f6271e5c44fd20ea27942d40d11b64a607
vladislav-karamfilov <vladislav.karamfilov@gmail.com>
2015-06-15 15:53:16 +0300

    Added ControllerExtensions to Web.Infrastructure project.

---
+++ src/Web/Bloggable.Web.Infrastructure/ControllerExtensions.cs
@@ -0,0 +1,12 @@
+﻿namespace Bloggable.Web.Infrastructure
+{
+    using System.Web.Mvc;
+
+    public static class ControllerExtensions
+    {
+        public static ActionResult EmptyResult(this Controller controller)
+        {
+            return new EmptyResult();
+        }
+    }
+}
