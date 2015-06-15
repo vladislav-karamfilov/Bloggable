53f6f2f50829a56934260c1303456dec039c4b0b
vladislav-karamfilov <vladislav.karamfilov@gmail.com>
2015-06-15 19:53:35 +0300

    Implemented AddXfoHeader and RemoveServerHeader modules.

---
+++ src/Web/Bloggable.Web.Infrastructure/Modules/AddXfoHeaderModule.cs
@@ -0,0 +1,26 @@
+﻿namespace Bloggable.Web.Infrastructure.Modules
+{
+    using System;
+    using System.Web;
+
+    public class AddXfoHeaderModule : IHttpModule
+    {
+        private const string XfoHeaderName = "X-Frame-Options";
+        private const string XfoHeaderValue = "SAMEORIGIN";
+
+        public void Init(HttpApplication context)
+        {
+            context.PreSendRequestHeaders += this.ContextOnPreSendRequestHeaders;
+        }
+
+        public void Dispose()
+        {
+        }
+
+        private void ContextOnPreSendRequestHeaders(object sender, EventArgs eventArgs)
+        {
+            var app = (HttpApplication)sender;
+            app.Context.Response.Headers[XfoHeaderName] = XfoHeaderValue;
+        }
+    }
+}
---
+++ src/Web/Bloggable.Web.Infrastructure/Modules/RemoveServerHeaderModule.cs
@@ -0,0 +1,25 @@
+﻿namespace Bloggable.Web.Infrastructure.Modules
+{
+    using System;
+    using System.Web;
+
+    public class RemoveServerHeaderModule : IHttpModule
+    {
+        private const string ServerHeaderName = "Server";
+
+        public void Init(HttpApplication context)
+        {
+            context.PreSendRequestHeaders += this.ContextOnPreSendRequestHeaders;
+        }
+
+        public void Dispose()
+        {
+        }
+
+        private void ContextOnPreSendRequestHeaders(object sender, EventArgs eventArgs)
+        {
+            var app = (HttpApplication)sender;
+            app.Context.Response.Headers.Remove(ServerHeaderName);
+        }
+    }
+}
