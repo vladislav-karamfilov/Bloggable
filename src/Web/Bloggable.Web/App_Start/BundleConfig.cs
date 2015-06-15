b59d17b537bb596ae071c11959226450e7acda57
vladislav-karamfilov <vladislav.karamfilov@gmail.com>
2015-06-15 12:44:59 +0300

    Organized the CSS and JS files and changed the Boostrap theme to Superhero.

--- src/Web/Bloggable.Web/App_Start/BundleConfig.cs
+++ src/Web/Bloggable.Web/App_Start/BundleConfig.cs
@@ -7,24 +7,34 @@
         // For more information on bundling, visit http://go.microsoft.com/fwlink/?LinkId=301862
         public static void RegisterBundles(BundleCollection bundles)
         {
+            RegisterScriptBundles(bundles);
+            RegisterStylesBundles(bundles);
+        }
+
+        private static void RegisterScriptBundles(BundleCollection bundles)
+        {
             bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
-                        "~/Scripts/jquery-{version}.js"));
+                "~/Scripts/jQuery/jquery-{version}.js",
+                "~/Scripts/jQuery/jquery.unobtrusive-ajax.js"));
 
             bundles.Add(new ScriptBundle("~/bundles/jqueryval").Include(
-                        "~/Scripts/jquery.validate*"));
+                "~/Scripts/jQuery-Validate/jquery.validate*"));
 
             // Use the development version of Modernizr to develop with and learn from. Then, when you're
             // ready for production, use the build tool at http://modernizr.com to pick only the tests you need.
             bundles.Add(new ScriptBundle("~/bundles/modernizr").Include(
-                        "~/Scripts/modernizr-*"));
+                "~/Scripts/Modernizr/modernizr-*"));
 
             bundles.Add(new ScriptBundle("~/bundles/bootstrap").Include(
-                      "~/Scripts/bootstrap.js",
-                      "~/Scripts/respond.js"));
+                "~/Scripts/Boostrap/bootstrap.js",
+                "~/Scripts/Respond/respond.js"));
+        }
 
+        private static void RegisterStylesBundles(BundleCollection bundles)
+        {
             bundles.Add(new StyleBundle("~/Content/css").Include(
-                      "~/Content/bootstrap.css",
-                      "~/Content/site.css"));
+                "~/Content/css/Bootstrap/Themes/Superhero/bootstrap-superhero.css",
+                "~/Content/css/site.css"));
         }
     }
 }
