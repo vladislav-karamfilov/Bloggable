9948b0a44f890b641c5cbc100942b24e01fc3a8e
vladislav-karamfilov <vladislav.karamfilov@gmail.com>
2015-06-18 14:49:33 +0300

    Implement DI registries (=== packages). Closes #1

---
+++ src/Web/Bloggable.Web.Config/ServiceRegistrationPackages/DataPackage.cs
@@ -0,0 +1,25 @@
+﻿namespace Bloggable.Web.Config.ServiceRegistrationPackages
+{
+    using System.Data.Entity;
+
+    using Bloggable.Common.Constants;
+    using Bloggable.Data;
+    using Bloggable.Data.Contracts;
+    using Bloggable.Data.Repositories.Base;
+
+    using SimpleInjector;
+    using SimpleInjector.Extensions;
+    using SimpleInjector.Packaging;
+
+    public class DataPackage : IPackage
+    {
+        public void RegisterServices(Container container)
+        {
+            container.RegisterPerWebRequest(() => new BloggableDbContext(AppSettingConstants.DefaultDbConnectionName));
+            container.RegisterPerWebRequest<DbContext>(container.GetInstance<BloggableDbContext>);
+
+            container.RegisterOpenGeneric(typeof(IDeletableEntityRepository<>), typeof(EfDeletableEntityRepository<>));
+            container.RegisterOpenGeneric(typeof(IRepository<>), typeof(EfRepository<>));
+        }
+    }
+}
--- src/Web/Bloggable.Web.Config/SimpleInjectorConfig.cs
+++ src/Web/Bloggable.Web.Config/SimpleInjectorConfig.cs
@@ -1,24 +1,9 @@
 namespace Bloggable.Web.Config
 {
-    using System.Collections.Generic;
-    using System.Data.Entity;
     using System.Reflection;
-    using System.Web;
     using System.Web.Mvc;
 
-    using Bloggable.Common.Constants;
-    using Bloggable.Data;
-    using Bloggable.Data.Contracts;
-    using Bloggable.Data.Models;
-    using Bloggable.Data.Repositories.Base;
-
-    using Microsoft.AspNet.Identity;
-    using Microsoft.AspNet.Identity.EntityFramework;
-    using Microsoft.Owin;
-
     using SimpleInjector;
-    using SimpleInjector.Advanced;
-    using SimpleInjector.Extensions;
     using SimpleInjector.Integration.Web.Mvc;
 
     public static class SimpleInjectorConfig
@@ -27,7 +12,7 @@ namespace Bloggable.Web.Config
         {
             var container = new Container();
 
-            InitializeContainer(container);
+            container.RegisterPackages(new[] { Assembly.GetExecutingAssembly() });
 
             container.RegisterMvcControllers(mvcControllersAssembly);
 
@@ -36,19 +21,6 @@ namespace Bloggable.Web.Config
             DependencyResolver.SetResolver(new SimpleInjectorDependencyResolver(container));
 
             return container;
-        }
-
-        private static void InitializeContainer(Container container)
-        {
-            container.RegisterPerWebRequest(() => new BloggableDbContext(AppSettingConstants.DefaultDbConnectionName));
-            container.RegisterPerWebRequest<IdentityDbContext<User>>(container.GetInstance<BloggableDbContext>);
-            container.RegisterPerWebRequest<DbContext>(container.GetInstance<BloggableDbContext>);
-            container.RegisterPerWebRequest<IUserStore<User>>(() => new UserStore<User>(container.GetInstance<IdentityDbContext<User>>()));
-            container.RegisterPerWebRequest(() => container.IsVerifying()
-                ? new OwinContext(new Dictionary<string, object>()).Authentication
-                : HttpContext.Current.GetOwinContext().Authentication);
-            container.RegisterOpenGeneric(typeof(IDeletableEntityRepository<>), typeof(EfDeletableEntityRepository<>));
-            container.RegisterOpenGeneric(typeof(IRepository<>), typeof(EfRepository<>));
         }
     }
 }
