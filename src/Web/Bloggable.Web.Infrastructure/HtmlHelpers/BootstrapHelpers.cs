2436d178f2687730756ce63fb6af168701a46899
vladislav-karamfilov <vladislav.karamfilov@gmail.com>
2015-06-20 21:18:07 +0300

    Added BootstrapHelper for label control and used it for posts administration.

---
+++ src/Web/Bloggable.Web.Infrastructure/HtmlHelpers/BootstrapHelpers.cs
@@ -0,0 +1,16 @@
+﻿namespace Bloggable.Web.Infrastructure.HtmlHelpers
+{
+    using System;
+    using System.Linq.Expressions;
+    using System.Web;
+    using System.Web.Mvc;
+    using System.Web.Mvc.Html;
+
+    public static class BootstrapHelpers
+    {
+        public static IHtmlString BootstrapLabelFor<TModel, TProp>(this HtmlHelper<TModel> helper, Expression<Func<TModel, TProp>> expression)
+        {
+            return helper.LabelFor(expression, new { @class = "control-label" });
+        }
+    }
+}
