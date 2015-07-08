26140384855690b183092e081466941023daa369
vladislav-karamfilov <vladislav.karamfilov@gmail.com>
2015-07-08 16:07:40 +0300

    Implemented ReflectionHelper and GetSubClasses helper method.

---
+++ src/Bloggable.Common/Helpers/ReflectionHelper.cs
@@ -0,0 +1,17 @@
+﻿namespace Bloggable.Common.Helpers
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Reflection;
+
+    public static class ReflectionHelper
+    {
+        public static IEnumerable<Type> GetSubClasses<T>(params Assembly[] assemblies)
+        {
+            return assemblies
+                .SelectMany(a => a.GetTypes())
+                .Where(type => type.IsSubclassOf(typeof(T)) && !type.IsAbstract);
+        }
+    }
+}
