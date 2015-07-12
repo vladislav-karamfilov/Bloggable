037d9c9c65c7d2f0624f199c0b2c97dc0505bd57
Vladislav Karamfilov <vladislav.karamfilov@gmail.com>
2015-07-12 11:36:15 +0300

    Implemented administration logs administration.

---
+++ src/Web/Bloggable.Web.Models/Administration/AdministrationLogs/ViewModels/AdministrationLogGridViewModel.cs
@@ -0,0 +1,28 @@
+﻿namespace Bloggable.Web.Models.Administration.AdministrationLogs.ViewModels
+{
+    using AutoMapper;
+
+    using Bloggable.Common.Mapping;
+    using Bloggable.Data.Models;
+
+    public class AdministrationLogGridViewModel : AdministrationGridViewModel, IMapFrom<AdministrationLog>, IHaveCustomMappings
+    {
+        public long Id { get; set; }
+
+        public string UserName { get; set; }
+
+        public string IpAddress { get; set; }
+
+        public string RequestType { get; set; }
+
+        public string Url { get; set; }
+
+        public string PostParams { get; set; }
+
+        public void CreateMappings(IConfiguration configuration)
+        {
+            configuration.CreateMap<AdministrationLog, AdministrationLogGridViewModel>()
+                .ForMember(m => m.UserName, opt => opt.MapFrom(e => e.User.UserName));
+        }
+    }
+}
---
+++ src/Web/Bloggable.Web/Areas/Administration/Controllers/AdministrationLogsController.cs
@@ -0,0 +1,33 @@
+﻿namespace Bloggable.Web.Areas.Administration.Controllers
+{
+    using System.Web.Mvc;
+
+    using Bloggable.Services.Administration.Contracts;
+    using Bloggable.Web.Areas.Administration.Controllers.Base;
+
+    using Kendo.Mvc.UI;
+
+    using EntityModel = Bloggable.Data.Models.AdministrationLog;
+    using ViewModel = Bloggable.Web.Models.Administration.AdministrationLogs.ViewModels.AdministrationLogGridViewModel;
+
+    public class AdministrationLogsController : KendoGridAdministrationController<EntityModel, ViewModel>
+    {
+        public AdministrationLogsController(IAdministrationService<EntityModel> administrationService)
+            : base(administrationService)
+        {
+        }
+
+        [HttpGet]
+        public ActionResult Index()
+        {
+            return this.View();
+        }
+
+        [HttpPost]
+        public ActionResult Destroy([DataSourceRequest]DataSourceRequest request, ViewModel model)
+        {
+            this.DestroyEntity(model.Id);
+            return this.GridOperation(request, model);
+        }
+    }
+}
