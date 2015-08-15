9eff9683dbaea16f66ddf21a2ac3fef8871f6db1
vladislav-karamfilov <vladislav.karamfilov@gmail.com>
2015-08-15 23:01:07 +0300

    Used username on login. Closes #4

--- src/Web/Bloggable.Web/Controllers/AccountController.cs
+++ src/Web/Bloggable.Web/Controllers/AccountController.cs
@@ -79,7 +79,7 @@
 
             // This doesn't count login failures towards account lockout
             // To enable password failures to trigger account lockout, change to shouldLockout: true
-            var result = await this.SignInManager.PasswordSignInAsync(model.Email, model.Password, model.RememberMe, shouldLockout: false);
+            var result = await this.SignInManager.PasswordSignInAsync(model.UserName, model.Password, model.RememberMe, shouldLockout: false);
             switch (result)
             {
                 case SignInStatus.Success:
@@ -146,7 +146,7 @@
         {
             if (this.ModelState.IsValid)
             {
-                var user = new User { UserName = model.Email, Email = model.Email };
+                var user = new User { UserName = model.UserName, Email = model.Email };
                 var result = await this.UserManager.CreateAsync(user, model.Password);
                 if (result.Succeeded)
                 {
