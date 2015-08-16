f3d686f70aee555e448c1c7df2011f4d3a58f1ca
vladislav-karamfilov <vladislav.karamfilov@gmail.com>
2015-08-16 15:51:09 +0300

    Fixed posting comment for blog post.

--- src/Web/Bloggable.Web.Config/RouteConfig.cs
+++ src/Web/Bloggable.Web.Config/RouteConfig.cs
@@ -24,7 +24,8 @@
             routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
-                defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional });
+                defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional },
+                namespaces: new[] { "Bloggable.Web.Controllers" });
         }
     }
 }
