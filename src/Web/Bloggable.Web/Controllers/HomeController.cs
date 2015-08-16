ed43bc5cd05a42a6801aeea95446621554c5c9a5
vladislav-karamfilov <vladislav.karamfilov@gmail.com>
2015-08-16 13:25:14 +0300

    Removed About and Contacts pages.

--- src/Web/Bloggable.Web/Controllers/HomeController.cs
+++ src/Web/Bloggable.Web/Controllers/HomeController.cs
@@ -34,19 +34,5 @@
 
             return this.View(posts);
         }
-
-        public ActionResult About()
-        {
-            this.ViewBag.Message = "Your application description page.";
-
-            return this.View();
-        }
-
-        public ActionResult Contact()
-        {
-            this.ViewBag.Message = "Your contact page.";
-
-            return this.View();
-        }
     }
 }
