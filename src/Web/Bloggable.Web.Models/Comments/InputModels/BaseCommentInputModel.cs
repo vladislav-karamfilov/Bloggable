a4fe8fae7f1017e0430f0448e92dd82e58a9cc8b
vladislav-karamfilov <vladislav.karamfilov@gmail.com>
2015-08-16 15:50:41 +0300

    Removed unnecessary AuthorId property in BaseCommentInputModel class.

--- src/Web/Bloggable.Web.Models/Comments/InputModels/BaseCommentInputModel.cs
+++ src/Web/Bloggable.Web.Models/Comments/InputModels/BaseCommentInputModel.cs
@@ -1,12 +1,7 @@
-﻿using System.ComponentModel.DataAnnotations;
-
-namespace Bloggable.Web.Models.Comments.InputModels
+﻿namespace Bloggable.Web.Models.Comments.InputModels
 {
     public abstract class BaseCommentInputModel
     {
         public string Content { get; set; }
-
-        [ScaffoldColumn(false)]
-        public string AuthorId { get; set; }
     }
 }
