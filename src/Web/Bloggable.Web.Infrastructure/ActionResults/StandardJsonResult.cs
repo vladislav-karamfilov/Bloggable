e16013306f67895877fcb0699388ddef07578677
vladislav-karamfilov <vladislav.karamfilov@gmail.com>
2015-08-17 09:37:20 +0300

    Progress on commenting posts #5

---
+++ src/Web/Bloggable.Web.Infrastructure/ActionResults/StandardJsonResult.cs
@@ -0,0 +1,73 @@
+﻿namespace Bloggable.Web.Infrastructure.ActionResults
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Net;
+    using System.Web;
+    using System.Web.Mvc;
+
+    using Bloggable.Common.Constants;
+
+    using Newtonsoft.Json;
+    using Newtonsoft.Json.Converters;
+    using Newtonsoft.Json.Serialization;
+
+    internal class StandardJsonResult : JsonResult
+    {
+        public ICollection<string> ErrorMessages { get; } = new List<string>();
+
+        public void AddError(string errorMessage)
+        {
+            this.ErrorMessages.Add(errorMessage);
+        }
+
+        public override void ExecuteResult(ControllerContext context)
+        {
+            if (context == null)
+            {
+                throw new ArgumentNullException(nameof(context));
+            }
+
+            if (this.JsonRequestBehavior == JsonRequestBehavior.DenyGet &&
+                string.Equals(context.HttpContext.Request.HttpMethod, "GET", StringComparison.OrdinalIgnoreCase))
+            {
+                throw new InvalidOperationException("GET access is not allowed. Change the JsonRequestBehavior if you need GET access.");
+            }
+
+            var response = context.HttpContext.Response;
+            response.ContentType = string.IsNullOrEmpty(this.ContentType) ? ContentTypeConstants.Json : this.ContentType;
+
+            if (this.ContentEncoding != null)
+            {
+                response.ContentEncoding = this.ContentEncoding;
+            }
+
+            this.SerializeData(response);
+        }
+
+        protected virtual void SerializeData(HttpResponseBase response)
+        {
+            if (this.ErrorMessages.Any())
+            {
+                var originalData = this.Data;
+                this.Data = new
+                {
+                    Success = false,
+                    OriginalData = originalData,
+                    this.ErrorMessages
+                };
+
+                response.StatusCode = (int)HttpStatusCode.BadRequest;
+            }
+
+            var settings = new JsonSerializerSettings
+            {
+                ContractResolver = new CamelCasePropertyNamesContractResolver(),
+                Converters = new JsonConverter[] { new StringEnumConverter(), },
+            };
+
+            response.Write(JsonConvert.SerializeObject(this.Data, settings));
+        }
+    }
+}
--- src/Web/Bloggable.Web.Infrastructure/Extensions/ControllerExtensions.cs
+++ src/Web/Bloggable.Web.Infrastructure/Extensions/ControllerExtensions.cs
@@ -1,12 +1,38 @@
 ﻿namespace Bloggable.Web.Infrastructure.Extensions
 {
+    using System.Linq;
     using System.Web.Mvc;
+
+    using Bloggable.Web.Infrastructure.ActionResults;
 
     public static class ControllerExtensions
     {
         public static ActionResult EmptyResult(this Controller controller)
+            => new EmptyResult();
+
+        public static ActionResult JsonSuccess<T>(this Controller controller, T data)
+            => new StandardJsonResult { Data = data };
+
+        public static JsonResult JsonError(this Controller controller, string errorMessage)
         {
-            return new EmptyResult();
+            var result = new StandardJsonResult();
+
+            result.AddError(errorMessage);
+
+            return result;
+        }
+
+        public static JsonResult JsonValidation(this Controller controller)
+        {
+            var result = new StandardJsonResult();
+
+            var validationMessages = controller.ModelState.Values.SelectMany(v => v.Errors.Select(e => e.ErrorMessage));
+            foreach (var validationMessage in validationMessages)
+            {
+                result.AddError(validationMessage);
+            }
+
+            return result;
         }
     }
 }
