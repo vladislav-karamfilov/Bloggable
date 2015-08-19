1c054f86d1c50e9e171ab8b3cd166632c5f75c6d
vladislav-karamfilov <vladislav.karamfilov@gmail.com>
2015-08-20 00:58:53 +0300

    Added global client-side error handling.

---
+++ src/Web/Bloggable.Web/Scripts/app/global.js
@@ -0,0 +1,9 @@
+﻿(function () {
+    window.onerror = function() {
+        if (Bloggable.Alerts) {
+            Bloggable.Alerts.error('There was a problem with your last action. Please reload the page and try again.');
+        } else {
+            alert('Something serious went wrong. Please close the application and try again.');
+        }
+    }
+})();
