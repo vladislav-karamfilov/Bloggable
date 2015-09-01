b842e437e07139a82bcaeb49fcb711cc7bd41dbd
vladislav-karamfilov <vladislav.karamfilov@gmail.com>
2015-09-01 18:44:37 +0300

    Started work on displaying static pages. #13

---
+++ src/Web/Bloggable.Web/Controllers/PagesController.cs
@@ -0,0 +1,13 @@
+﻿namespace Bloggable.Web.Controllers
+{
+    using System;
+    using System.Web.Mvc;
+
+    public class PagesController : BaseController
+    {
+        public ActionResult Page(string id)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
