7ac5b2214ef2f38b01033a138d94b61d2b4d84e2
vladislav-karamfilov <vladislav.karamfilov@gmail.com>
2015-09-13 16:14:24 +0300

    Implemented simple error pages.

---
+++ src/Web/Bloggable.Web/Controllers/ErrorsController.cs
@@ -0,0 +1,11 @@
+﻿namespace Bloggable.Web.Controllers
+{
+    using System.Web.Mvc;
+
+    public class ErrorsController : BaseController
+    {
+        public ActionResult General() => this.View();
+
+        public ActionResult PageNotFound() => this.View();
+    }
+}
