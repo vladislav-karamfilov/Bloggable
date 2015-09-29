1ff47042ad496de972b1c3e7328c066b3d479b7f
vladislav-karamfilov <vladislav.karamfilov@gmail.com>
2015-09-29 23:47:41 +0300

    Started implementations of displaying static page. #13

---
+++ src/Web/Bloggable.Web.Models/Pages/ViewModels/PageDetailsViewModel.cs
@@ -0,0 +1,22 @@
+﻿namespace Bloggable.Web.Models.Pages.ViewModels
+{
+    using System;
+
+    using Bloggable.Common.Mapping;
+    using Bloggable.Data.Models;
+
+    public class PageDetailsViewModel : IMapFrom<Page>
+    {
+        public int Id { get; set; }
+
+        public string Title { get; set; }
+        
+        public string Content { get; set; }
+
+        public string MetaDescription { get; set; }
+
+        public string MetaKeywords { get; set; }
+
+        public DateTime? ModifiedOn { get; set; }
+    }
+}
